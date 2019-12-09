1a6063bd9faef5e03ced80afad6d75d45a521eb4
allen00852 <allenczr@gmail.com>
2019-12-09 17:06:19 +0800

    常量

---
+++ Demo01changliang.java
@@ -0,0 +1,29 @@
+/**
+ * Demo01changliang
+ */
+public class Demo01changliang {
+
+    public static void main(String[] args) {
+        // 字符串常量 双引号的为字符串常量
+        System.out.println("abc");
+        System.out.println(""); // 可以留空字符串常量
+        System.out.println("efd");
+
+        // 整数常量 以整数数字
+        System.out.println(10);
+        System.out.println(-10);
+
+        // 浮点数常量 以小数点
+        System.out.println(3.14);
+        System.out.println(-2.5);
+
+        // 字符常量 不能为'' 不能两个字符'aa' 以单引号
+        System.out.println('A');
+        System.out.println('B');
+        System.out.println('C');
+
+        // 布尔常量
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
