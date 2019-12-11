72ee135967d99b30ae9f3fefa23db21ab5eefcae
allen00852 <allenczr@gmail.com>
2019-12-11 18:46:28 +0800

    赋值运算符

---
+++ Demo08fuzhiyunsuanfu.java
@@ -0,0 +1,32 @@
+/*
+赋值运算符
+    基本运算符
+        int a = 30;
+    符合运算符
+        +=  a += 2  相当于  a = a + 2
+        -=  a -= 2  相当于  a = a - 2
+        *=  a *= 2  相当于  a = a * 2
+        /=  a /= 2  相当于  a = a / 2
+        %=  a %= 2  相当于  a = a % 2
+*/
+public class Demo08fuzhiyunsuanfu {
+
+    public static void main(String[] args) {
+        int a = 10;
+        // 相当于 a = 10 + 6
+        System.out.println(a += 6); // 16
+
+        int b = 100;
+        // 相当于 b = 100 % 33
+        System.out.println(b %= 33); // 1
+
+        byte c = 30;
+        c += 3;
+        // 这是语言帮我们自动做的强制转换
+        // c = byte + int
+        // c = int + int
+        // c = int
+        // c = （byte）int
+        System.out.println(c);
+    }
+}
